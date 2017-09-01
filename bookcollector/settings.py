db36492677eb01733a8eb383ccaa860b409d9de0
jessica-thatcher <jessica.thatcher@imtapps.com>
2017-09-01 10:38:10 -0500

    add landing page to redirect to apps

--- bookcollector/settings.py
+++ bookcollector/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'bookcollection.apps.BookcollectionConfig',
     'gamecollection.apps.GamecollectionConfig',
+    'landingpage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
