04289f1e4055143d8afa12979854817d2482484d
Jessica Thatcher <jthatcher1293@gmail.com>
2017-09-18 10:20:40 -0500

    Update settings.py

--- bookcollector/settings.py
+++ bookcollector/settings.py
@@ -117,3 +117,17 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/bookcollection/'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
