7f3ae1dbe0877f4fdcd4aa1499aea01cf55793d9
Jessica Thatcher <jthatcher1293@gmail.com>
2017-09-18 10:18:52 -0500

    Update settings.py

--- bookcollector/settings.py
+++ bookcollector/settings.py
@@ -66,25 +66,16 @@ WSGI_APPLICATION = 'bookcollector.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'collector',
+        'USER': 'jthatcher',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-}
-
-DEBUG = True
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'collector',
-#         'USER': 'jthatcher',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-#   }
+  }
 
 
 # Password validation
