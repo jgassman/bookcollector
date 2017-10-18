1323264c5efdc490897d717b45c518f4969e3ec3
jthatcher1293 <jthatcher1293@gmail.com>
2017-10-18 09:29:49 -0500

    add models for moviecollection

--- moviecollection/models.py
+++ moviecollection/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=25)
+
+
+class Series(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    year = models.IntegerField()
+    series = models.ForeignKey(Series)
+    series_number = models.IntegerField(null=True, blank=True)
+    genre = models.ForeignKey(Genre)
+
+    class Meta:
+        unique_together = ('title', 'year',)
+        ordering = ['title']
