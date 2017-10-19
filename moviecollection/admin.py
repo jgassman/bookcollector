cb737d32c0ccef67810b596d2569596159d92516
jthatcher1293 <jthatcher1293@gmail.com>
2017-10-19 12:36:56 -0500

    add tags (moviecollection)

--- moviecollection/admin.py
+++ moviecollection/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Genre, Series, Movie, Subgenre
+from .models import Genre, Series, Movie, Subgenre, Tag
 
 
 admin.site.register(Genre)
 admin.site.register(Subgenre)
 admin.site.register(Series)
 admin.site.register(Movie)
+admin.site.register(Tag)
