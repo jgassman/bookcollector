f1c5d3970d4fec6de01729523009e84ac50d3801
jwilson1293 <jthatcher1293@gmail.com>
2018-12-12 15:26:14 -0600

    add storage action to book admin

--- bookcollection/admin.py
+++ bookcollection/admin.py
@@ -4,11 +4,19 @@ from .forms import BookForm
 from .models import Author, Book, Series, Genre, Subgenre, Tag
 
 
+def store_books(BookAdmin, request, queryset):
+    for book in queryset:
+        book.storage = True
+        book.save()
+store_books.short_description = 'Move to Storage'
+
+
 class BookAdmin(admin.ModelAdmin):
     form = BookForm
 
     list_filter = ('series',)
     search_fields = ('title',)
+    actions = [store_books, ]
 
 
 admin.site.register(Author)
