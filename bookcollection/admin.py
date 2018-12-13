049d0896252da4a148efe922071d001b831f1e80
jwilson1293 <jthatcher1293@gmail.com>
2018-12-13 15:52:06 -0600

    update series admin

--- bookcollection/admin.py
+++ bookcollection/admin.py
@@ -4,11 +4,20 @@ from .forms import BookForm
 from .models import Author, Book, Series, Genre, Subgenre, Tag
 
 
+def store_all(queryset):
+    for obj in queryset:
+        obj.storage = True
+        obj.save()
+
+
 def store_books(BookAdmin, request, queryset):
-    for book in queryset:
-        book.storage = True
-        book.save()
+    store_all(queryset)
 store_books.short_description = 'Move to Storage'
+
+
+def store_series(SeriesAdmin, request, queryset):
+    store_all(queryset)
+store_series.short_description = 'Move to Storage'
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -18,10 +27,15 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title',)
     actions = [store_books, ]
 
+class SeriesAdmin(admin.ModelAdmin):
+
+    search_fields = ('name',)
+    actions = [store_series, ]
+
 
 admin.site.register(Author)
 admin.site.register(Book, BookAdmin)
-admin.site.register(Series)
+admin.site.register(Series, SeriesAdmin)
 admin.site.register(Genre)
 admin.site.register(Subgenre)
 admin.site.register(Tag)
